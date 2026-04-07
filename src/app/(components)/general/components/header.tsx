97d275c87f28f4cece602ad69d5227cec36bbe79
gabriel felipe <gabrielferreiro963@gmail.com>
2026-04-07 17:15:38 -0300

    made: header basic style

--- src/app/(components)/general/components/header.tsx
+++ src/app/(components)/general/components/header.tsx
@@ -1,8 +1,59 @@
 import styles from "../styles/_header.module.scss"
 
-function header() {
+const Item = ({indice, value}: {indice: string, value: number}) =>{
+  return(<>
+    <li className={
+      indice != "total" 
+        ? styles.indexCardOthers 
+        : styles.indexCardTotal}
+        data-cor={indice}>
+      <p>{value}</p>
+      <span>{indice.toUpperCase()}</span>
+    </li>
+  </>)
+}
+
+type Data = {
+  index: string,
+  value: number
+}
+
+async function header() {
+
+  const datas: Data[] = [
+    {
+      index: "pessimos",
+      value: 3
+    },
+    {
+      index: "ruins",
+      value: 11
+    },
+    {
+      index: "bons",
+      value: 4
+    },
+    {
+      index: "perfeitos",
+      value: 14
+    },
+    {
+      index: "total",
+      value: 60
+    }
+  ]
+
   return (
-    <header id={styles.header}>header</header>
+    <header id={styles.header}>
+      <ul>
+        {datas.map((x, index) => 
+        <Item 
+          key={index}
+          indice={x.index} 
+          value={x.value}
+        />)}
+      </ul>
+    </header>
   )
 }
 
