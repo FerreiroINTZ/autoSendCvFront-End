88533a669ff594a522a1083508adc0f47045147d
gabriel felipe <gabrielferreiro963@gmail.com>
2026-04-08 19:14:32 -0300

    made: vancany card title

--- src/app/(components)/listage/card.tsx
+++ src/app/(components)/listage/card.tsx
@@ -1,15 +1,48 @@
+import Image from "next/image"
+
 import styles from "./styles/_card.module.scss"
 import {Vancancy} from "../../(functions)/getList"
 
+import linkIcon from "@icons/link.png"
+
 function card({
   title, 
-  paridade
+  paridade,
+  area,
+  regiao,
+  link
 }: Vancancy) {
   return (
-    <div className={styles.cardContainer} data-paridade={`${paridade}`}>
-      <h3>{title}</h3>
-      <span className={styles.cardLight}></span>
-    </div>
+    <>
+    <input className={styles.radionBuntonVac} type="radio" id={title} name="vancanciesCards"/>
+    <label htmlFor={title} className={styles.cardContainer} data-paridade={`${paridade}`}>
+      
+      <div className={styles.mainInfos}>
+        
+        <div className={styles.titleContainer}>
+          <h3 className={styles.title}>{title}</h3>
+          <p className={styles.area_regiao}>{area} - {regiao}</p>
+          <a href={link} className={styles.link}>
+            <Image src={linkIcon} alt="link icon" title={link}/>
+          </a>
+        </div>
+        
+        <ul className={styles.infos}>
+          <li>Gabriel</li>
+        </ul>
+        
+        <span className={styles.cardLight}></span>
+      </div>
+
+      <div className={styles.expendendInfos}>
+          <div className={styles.block}>
+            <p className={styles.label}>SUMARY</p>
+            <p className={styles.value}>Boa vaga geral, mas não específica para sua stack.</p>
+          </div>
+      </div>
+      
+    </label>
+    </>
   )
 }
 
